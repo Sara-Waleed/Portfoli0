ece8a1449a23f0d129479c654a904e07433d1209
Sara Waleed <sarawaleedmohamed2@gmail.com>
2025-06-23 00:59:40 +0300

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/your-repo-name/", // This will be dynamically updated by GitHub Actions
+  base: "/", // This will be dynamically updated by GitHub Actions
 });
